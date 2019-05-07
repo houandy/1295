8bc4d7af4e1589ba599f3a9ddf9c42a822cfb1e1
ariel fang <ariel.fang@qsitw.com>
2019-05-07 16:16:17 +0800

    update U-boot LED status for hvlrs

--- Bootcode/U-Boot64-RTD129x/build.sh
+++ Bootcode/U-Boot64-RTD129x/build.sh
@@ -3,9 +3,10 @@
 
 #rm -rf DVRBOOT_OUT
 #[ $# = 0 ] && echo -e "Usage:\n\t./build.sh [RTD16xx_spi|RTD16xx_emmc|RTD12xx_emmc]\n\t./build.sh\t=>For building all version"
-[ $# = 0 ] && echo -e "Usage:\n\t./build.sh [RTD16xx_spi|RTD16xx_emmc|RTD129x_spi|RTD129x_emmc]" && exit 0
+[ $# != 2 ] && echo -e "Usage:\n\t./build.sh [RTD16xx_spi|RTD16xx_emmc|RTD129x_spi|RTD129x_emmc] [project]" && exit 0
 target=$1
-
+project=$2
+patch -p1 < patches/${project}/*.patch
 ########### Build Thor A01 RTK #############
 #BUILD_HWSETTING_LIST=`ls $HWSETTING_DIR`
 #BUILD_HWSETTING_LIST=RTD161x_hwsetting_BOOT_2DDR4_8Gb_s1600 RTD161x_hwsetting_BOOT_2DDR4_8Gb_s2400 RTD161x_hwsetting_BOOT_2DDR4_8Gb_s2133 RTD161x_hwsetting_BOOT_2DDR4_8Gb_s2666 RTD161x_hwsetting_BOOT_LPDDR4_16Gb_s3200_H 
@@ -158,3 +159,4 @@ if [ $target = RTD129x_spi ]; then
 	done
 
 fi
+patch -R -p1 < patches/${project}/*.patch
--- Bootcode/U-Boot64/build.sh
+++ Bootcode/U-Boot64/build.sh
@@ -3,9 +3,10 @@
 
 #rm -rf DVRBOOT_OUT
 #[ $# = 0 ] && echo -e "Usage:\n\t./build.sh [RTD16xx_spi|RTD16xx_emmc|RTD12xx_emmc]\n\t./build.sh\t=>For building all version"
-[ $# = 0 ] && echo -e "Usage:\n\t./build.sh [RTD16xx_spi|RTD16xx_emmc]" && exit 0
+[ $# != 2 ] && echo -e "Usage:\n\t./build.sh [RTD16xx_spi|RTD16xx_emmc] [project]" && exit 0
 target=$1
-
+project=$2
+patch -p1 < patches/${project}/*.patch
 ########### Build Thor A00 RTK #############
 #BUILD_HWSETTING_LIST=`ls $HWSETTING_DIR`
 #BUILD_HWSETTING_LIST=RTD161x_hwsetting_BOOT_2DDR4_8Gb_s1600 RTD161x_hwsetting_BOOT_2DDR4_8Gb_s2400 RTD161x_hwsetting_BOOT_2DDR4_8Gb_s2133 RTD161x_hwsetting_BOOT_2DDR4_8Gb_s2666 RTD161x_hwsetting_BOOT_LPDDR4_16Gb_s3200_H 
@@ -71,4 +72,4 @@ if [ $target = RTD129x_emmc ]; then
 	cp ./examples/flash_writer/image/hw_setting/RTD1296_hwsetting_BOOT_4DDR4_4Gb_s1866.bin ./DVRBOOT_OUT/$target/hw_setting/
 	cp ./examples/flash_writer/dvrboot.exe.bin ./DVRBOOT_OUT/$target/NAS_1296_B00_emmc_1866_DDR4_4X4Gb.bin
 fi
-
+patch -R -p1 < patches/${project}/*.patch
