e3dc4318533e9e163da35789bbb99d15cfe18558
Mars Lin <mars.lin@qsitw.com>
2019-11-18 11:39:08 +0800

    update fan define

--- OpenWRT-LEDE/package/utils/fan/files/sbin/fan.sh
+++ OpenWRT-LEDE/package/utils/fan/files/sbin/fan.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-temperature=40
+hdd_temp=40
+soc_temp=80
 hddthermal=
+socthermal=
 high_speed=50
 low_speed=35
 
 fan()
 {
 	hddthermal=$(thermal hdd | grep Temp | awk -F " " '{print $2}')
+	socthermal=$(thermal soc | grep Temp | awk -F "=" '{print $2}' | awk -F "." '{print $1}')
 	dutyrate=$(cat /sys/devices/platform/980070d0.pwm/dutyRate0  | cut -d "%" -f1)
 	[ $hddthermal -gt "20" ] || return
+	[ $socthermal -gt "40" ] || return
 
-	if [ "$hddthermal" -ge "$temperature" ]; then
+	if [ "$hddthermal" -ge "$hdd_temp" ] || [ "$socthermal" -ge "$soc_temp" ] ; then
 		if [ "$dutyrate" -lt "$high_speed" ]; then
 			[ -f /tmp/fan_high ] &&	{
 				echo $high_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
