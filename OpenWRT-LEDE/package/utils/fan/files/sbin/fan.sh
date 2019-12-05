6338aa1ce68318e6a0b8822cd9a95ae282d9ba3c
Mars Lin <mars.lin@qsitw.com>
2019-12-05 10:20:03 +0800

    update fan and fixed read/write HDD throughput issue

--- OpenWRT-LEDE/package/utils/fan/files/sbin/fan.sh
+++ OpenWRT-LEDE/package/utils/fan/files/sbin/fan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+hdd_temp_file="/tmp/hddthermal"
+get_hdd_temp_count_file="/tmp/hddthermalcount"
 hdd_temp=40
 soc_temp=80
 hdd_limit_temp=48
@@ -8,32 +10,43 @@ socthermal=
 fast_speed=70
 high_speed=50
 low_speed=30
+count=1
+min=
+
+get_hdd_temp()
+{
+	hddthermal=$(thermal hdd | grep Temp | awk -F " " '{print $2}')
+	echo $hddthermal > ${hdd_temp_file}
+	echo 1 > ${get_hdd_temp_count_file}
+}
 
 fan()
 {
-	hddthermal=$(thermal hdd | grep Temp | awk -F " " '{print $2}')
 	socthermal=$(thermal soc | grep Temp | awk -F "=" '{print $2}' | awk -F "." '{print $1}')
+	if [ -f $hdd_temp_file ] ; then
+		hddthermal=$(cat ${hdd_temp_file})
+		c1=$(($hdd_temp-$hddthermal))
+		c2=$(($hdd_limit_temp-$hddthermal))
+		cc1=${c1#-}
+		cc2=${c2#-}
+		[ $cc1 -le $cc2 ] && min=$cc1 || min=$cc2
+		cc=$(($min*2))
+		[ $1 -ge $cc ] && get_hdd_temp
+	else
+		get_hdd_temp
+	fi
+
 	dutyrate=$(cat /sys/devices/platform/980070d0.pwm/dutyRate0  | cut -d "%" -f1)
-	[ $hddthermal -gt "20" ] || return
-	[ $socthermal -gt "40" ] || return
+	[ $hddthermal -gt "20" ] || return 0
+	[ $socthermal -gt "40" ] || return 0
 
 	if [ "$hddthermal" -ge "$hdd_temp" ] || [ "$socthermal" -ge "$soc_temp" ] ; then
 		if [ "$dutyrate" -lt "$high_speed" ]; then
-			[ -f /tmp/fan_high ] &&	{
-				echo $high_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
-			} || {
-				touch /tmp/fan_high
-				return
-			}
+			echo $high_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
 		else
 			if [ "$hddthermal" -ge "$hdd_limit_temp" ]; then
 				if [ "$dutyrate" -lt "$fast_speed" ]; then
-					[ -f /tmp/fan_fast ] &&	{
-						echo $fast_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
-					} || {
-						touch /tmp/fan_fast
-						return
-					}
+					echo $fast_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
 				fi
 			else
 				if [ "$dutyrate" -eq "$fast_speed" ]; then
@@ -43,17 +56,17 @@ fan()
 		fi
 	else
 		if [ "$dutyrate" -gt "$low_speed" ]; then
-			[ -f /tmp/fan_low ] && {
-				echo $low_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
-			} || {
-				touch /tmp/fan_low
-				return
-			}
+			echo $low_speed > /sys/devices/platform/980070d0.pwm/dutyRate0
 		fi
 	fi
-	rm /tmp/fan_* > /dev/null 2>&1
 }
 
-fan
-exit 0
+if [ -f ${get_hdd_temp_count_file} ]; then
+	count=$(cat $get_hdd_temp_count_file)
+	echo $((count+1)) > $get_hdd_temp_count_file
+else
+	echo 1 > ${get_hdd_temp_count_file}
+fi
+
+fan $count
 
--- OpenWRT-LEDE/package/utils/fan/src/fan.c
+++ OpenWRT-LEDE/package/utils/fan/src/fan.c
@@ -1,48 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>  
-#include <sys/stat.h>  
-#include <fcntl.h>
+#include <stdlib.h>
 
-#define CMD_RESULT_LEN 256
-
-static int execute_cmd_result(const char *cmd, char *result)
-{       
-        if (cmd == NULL || result == NULL || strlen(cmd) <= 0)
-                return -1;
-        char buf_ps[CMD_RESULT_LEN] = {0};
-        char ps[CMD_RESULT_LEN] = {0};
-        FILE *ptr; 
-        strcpy(ps, cmd);
-        if((ptr=popen(ps, "r"))!=NULL)
-        {       
-                while(fgets(buf_ps, CMD_RESULT_LEN, ptr)!=NULL)
-                {       
-                        strcat(result, buf_ps);
-                        if(strlen(result)>CMD_RESULT_LEN)
-                                break;
-                }
-                pclose(ptr);
-                ptr = NULL;
-        }
-        else
-        {       
-                printf("error: [sys.c]popen %s error\n", ps);
-                return -1;
-        }
-        return 0;
-}
 
 int main(int argc, char* argv[])
 {
-        char buff[CMD_RESULT_LEN] = {0};
         while(1){
-                execute_cmd_result("/sbin/fan.sh", buff);
-                sleep(3);
+                system("/sbin/fan.sh");
+                sleep(10);
         }
-	return 0; 
+        return 0;
 }
-
